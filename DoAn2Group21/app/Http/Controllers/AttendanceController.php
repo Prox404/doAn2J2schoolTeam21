5e725cdfc15584dc75e515ea4d1b700aff7415e4
captainprox <92608546+captainprox02@users.noreply.github.com>
2022-09-08 20:20:22 +0700

    edit ui for history

--- DoAn2Group21/app/Http/Controllers/AttendanceController.php
+++ DoAn2Group21/app/Http/Controllers/AttendanceController.php
@@ -27,32 +27,6 @@ class AttendanceController extends Controller
         View::share('title', $title);
     }
 
-    public function index()
-    {
-        $subjects = Subjects::query()
-        ->addSelect('subjects.*')
-        ->get();
-
-        return view('attendance.index')->with('subjects', $subjects);
-    }
-
-    public function api()
-    {
-        $query = Classes::query()
-            ->addSelect('classes.*')
-            ->addSelect('subjects.name as subject_name')
-            ->leftJoin('subjects', 'classes.subject_id', 'subjects.id');
-        
-        return DataTables::of($query)
-        ->addColumn('history', function ($object) {
-            return route('attendance.history', $object);
-        })
-        ->addColumn('destroy', function ($object) {
-            return route('attendance.destroy', $object);
-        })
-        ->make(true);
-    }
-
     public function findValueById($object, $id, $id_field, $field)
     {
         foreach ($object as $key => $value) {
@@ -62,10 +36,87 @@ class AttendanceController extends Controller
         }
     }
 
+    public function index()
+    {
+        if (auth()->user()->level <= 4 && auth()->user()->level >= 2) {
+            $subjects = Subjects::query()
+                ->addSelect('subjects.*')
+                ->get();
+
+            return view('attendance.index')->with('subjects', $subjects);
+        }
+        if (auth()->user()->level == 1) {
+            $id = auth()->user()->id;
+            $classes = ClassStudent::query()
+                ->addSelect('class_id')
+                ->where('user_id', $id)
+                ->distinct('class_id')
+                ->get();
+
+            $class_infor = Classes::whereIn('id', $classes)->get();
+
+            $schedules = Schedules::whereIn('class_id', $classes)->get();
+            $scheduleByClass = [];
+            $schedule_id = [];
+            
+            foreach ($schedules as $schedule) {
+                $schedule_id[] = $schedule['id'];
+                $schedule['shift'] = $this->findValueById($class_infor, $schedule['class_id'], 'id', 'shift');
+                $schedule['name'] = $this->findValueById($class_infor, $schedule['class_id'], 'id', 'name');
+            }
+
+            $attendances = Attendance::whereIn('schedule_id', $schedule_id)->where('user_id', $id)->get();
+
+            foreach ($attendances as $attendance){
+                foreach($schedules as $schedule){
+                    if($attendance['schedule_id'] == $schedule['id']){
+                        switch($attendance['status']){
+                            case 1:
+                                $schedule['status'] = 'Present';
+                                break;
+                            case 2:
+                                $schedule['status'] = 'Absent';
+                                break;
+                            case 3:
+                                $schedule['status'] = 'OnLeave';
+                                break;
+                            default:
+                                $schedule['status'] = 'Present';
+                                break;
+                        }
+                    }else{
+                        $schedule['status'] = NULL;
+                    }
+                }
+            }
+
+            foreach ($schedules as $schedule) {
+                $scheduleByClass[$schedule['name']][] = $schedule;
+            }
+            
+            return view('attendance.index')->with('scheduleByClass', $scheduleByClass);
+        }
+
+    }
+
+    public function api()
+    {
+        $query = Classes::query()
+            ->addSelect('classes.*')
+            ->addSelect('subjects.name as subject_name')
+            ->leftJoin('subjects', 'classes.subject_id', 'subjects.id');
+
+        return DataTables::of($query)
+            ->addColumn('history', function ($object) {
+                return route('attendance.history', $object);
+            })
+            ->make(true);
+    }
+
     public function history(Classes $class)
     {
         $schedules = Schedules::with('classes')->where('class_id', $class->id)->get();
-        
+
         $schedule_id = [];
         foreach ($schedules as $key => $value) {
             $schedule_id[] = $value->id;
@@ -78,7 +129,7 @@ class AttendanceController extends Controller
             ->whereIn('schedule_id', $schedule_id)
             ->get();
 
-        
+
         $all_students = [];
         $students_absent = [];
         $students_present = [];
@@ -98,9 +149,9 @@ class AttendanceController extends Controller
         $students_present = array_count_values($students_present);
 
         $students_absent_array = [];
-        
+
         foreach ($students_absent as $key => $value) {
-            if($value > 3){
+            if ($value > 3) {
                 $students_absent_array[] = [
                     'id' => $key,
                     'name' => $this->findValueById($attendance, $key, 'user_id', 'name'),
@@ -110,7 +161,7 @@ class AttendanceController extends Controller
         }
         $students_present_array = [];
         foreach ($students_present as $key => $value) {
-            if($value == count($attendances)){
+            if ($value == count($attendances)) {
                 $students_present_array[] = [
                     'id' => $key,
                     'name' => $this->findValueById($attendance, $key, 'user_id', 'name'),
@@ -127,26 +178,26 @@ class AttendanceController extends Controller
 
 
         $line_chart_labels = [];
-        foreach($schedules as $schedule) {
+        foreach ($schedules as $schedule) {
             $line_chart_labels[] = $schedule->date;
         }
 
         $line_chart_data = [];
-        foreach($line_chart_labels as $label) {
-            if(in_array($label, array_keys($attendances_data))) {
-                foreach($attendances_data as $key => $value) {
-                    if($key === $label) {
-                        if(!empty($value[1])) {
+        foreach ($line_chart_labels as $label) {
+            if (in_array($label, array_keys($attendances_data))) {
+                foreach ($attendances_data as $key => $value) {
+                    if ($key === $label) {
+                        if (!empty($value[1])) {
                             $present = $value[1];
                         } else {
                             $present = 0;
                         }
-                        if(!empty($value[2])) {
+                        if (!empty($value[2])) {
                             $absent = $value[2];
                         } else {
                             $absent = 0;
                         }
-                        if(!empty($value[3])) {
+                        if (!empty($value[3])) {
                             $onLeave = $value[3];
                         } else {
                             $onLeave = 0;
@@ -173,26 +224,25 @@ class AttendanceController extends Controller
         foreach ($line_chart_data as $data) {
             $absent_students[] = $data['absent'];
             $present_students[] = $data['present'];
-            $onLeave_students[] = $data['onLeave'];  
+            $onLeave_students[] = $data['onLeave'];
         }
 
 
         return view('attendance.history')
-        ->with('schedules', $schedules)
-        ->with('class', $class)
-        ->with('numberStudentAbsentMoreThan3Sessions', $students_absent_array)
-        ->with('numberStudentPresentFullDay', $students_present_array)
-        ->with('allStudent', $all_students)
-        ->with('line_chart_labels', $line_chart_labels)
-        ->with('numberStudentPresent', $present_students)
-        ->with('numberStudentAbsent', $absent_students)
-        ->with('numberStudentOnLeave', $onLeave_students);
+            ->with('schedules', $schedules)
+            ->with('class', $class)
+            ->with('numberStudentAbsentMoreThan3Sessions', $students_absent_array)
+            ->with('numberStudentPresentFullDay', $students_present_array)
+            ->with('allStudent', $all_students)
+            ->with('line_chart_labels', $line_chart_labels)
+            ->with('numberStudentPresent', $present_students)
+            ->with('numberStudentAbsent', $absent_students)
+            ->with('numberStudentOnLeave', $onLeave_students);
 
-        // return $students_present_array;
     }
 
 
-    public function attendance( $class_id , $schedule_id)
+    public function attendance($class_id, $schedule_id)
     {
         $students = ClassStudent::query()
             ->addSelect('users.id', 'users.name', 'class_students.*')
@@ -200,13 +250,12 @@ class AttendanceController extends Controller
             ->where('class_id', '=', $class_id)
             ->get();
         $schedules = Schedules::find($schedule_id);
-        
-        return view('attendance.attendance')
-        ->with('students', $students)
-        ->with('schedules', $schedules)
-        ->with('class_id', $class_id);
 
-        // return $schedules;
+        return view('attendance.attendance')
+            ->with('students', $students)
+            ->with('schedules', $schedules)
+            ->with('class_id', $class_id);
+
     }
 
     public function store(Request $request)
@@ -219,16 +268,16 @@ class AttendanceController extends Controller
                     ->where('schedule_id', $schedule_id)
                     ->count();
 
-                if($find >= 1){
+                if ($find >= 1) {
                     Attendance::where('user_id', $key)
-                    ->where('schedule_id', $schedule_id)
-                    ->update([
-                        'status' => $value,
-                    ]);
-                }else{
+                        ->where('schedule_id', $schedule_id)
+                        ->update([
+                            'status' => $value,
+                        ]);
+                } else {
                     Attendance::Create([
-                        'user_id'=> $key,
-                        'schedule_id'=> $schedule_id,
+                        'user_id' => $key,
+                        'schedule_id' => $schedule_id,
                         'status' => $value,
                     ]);
                 }
@@ -238,6 +287,5 @@ class AttendanceController extends Controller
             return redirect()->route('attendance.index')->with('message', 'Error!!! ' . $e->getMessage());
         }
 
-        // return $request;
     }
 }
